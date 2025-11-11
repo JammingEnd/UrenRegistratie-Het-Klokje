3a5f15662eac24496005831aaf81b4e3ed41e976
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2025-11-11 12:50:25 +0100

    ini
    Basis Opstelling voor de App

---
+++ HetKlokje/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HetKlokje
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HetKlokje/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HetKlokje;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
