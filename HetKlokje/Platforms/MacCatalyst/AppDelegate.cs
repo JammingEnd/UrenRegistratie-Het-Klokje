5aa3949bf837b8b8bdc85217ba2475ed34ff2b30
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2025-11-12 11:31:32 +0100

    reset

--- HetKlokje/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace HetKlokje;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
